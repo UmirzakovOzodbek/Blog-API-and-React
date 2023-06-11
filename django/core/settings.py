300cc25b4936229528981097090b5ceea531e81e
user <ozodbekumurzakov2006@gmail.com>
2023-06-11 21:05:03 +0500

    Initial commit .gitignore

--- django/core/settings.py
+++ django/core/settings.py
@@ -189,5 +189,10 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
 
